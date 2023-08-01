83b1d299c3225195c5ba86bd49e738fd188db1d6
yoex <ytentertainmnet94@gmail.com>
2023-08-01 10:18:53 -0400

    checkpoint antes de agregar foundry devops

---
+++ script/Interactions.s.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+
+contract FundFundMe is Script {
+    function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        //Si en la estructura tuvieramos mas valores, tendremiamos que recibilors asi (address ethUsd, address  2,) etc
+
+        vm.startBroadcast();
+
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+
+        return fundMe;
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        //Si en la estructura tuvieramos mas valores, tendremiamos que recibilors asi (address ethUsd, address  2,) etc
+
+        vm.startBroadcast();
+
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+
+        return fundMe;
+    }
+}
